ed04ced23ac8681ee22fcd0b205e975ad22dc1ad
eko0 <eko00.5177@gmail.com>
2025-10-25 18:11:31 +0200

    test before liquid class

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import ThemeToggle from "./ui/items/ThemeToggle";
 import Footer from "./ui/component/Footer";
+import Nav from "./ui/component/Nav";
 import { body, path } from "framer-motion/client";
 import { useEffect } from "react";
 import React from "react";
@@ -9,6 +10,7 @@ import React from "react";
 export default function Home() {
   return (
     <div className="page">
+      <Nav/>
       {/* add nav later */}
       <div className="first-section">
         <img
---
+++ src/components/ui/component/Nav.jsx
@@ -0,0 +1,20 @@
+import { motion } from "framer-motion";
+import { body, path } from "framer-motion/client";
+import { useEffect } from "react";
+import React from "react";
+
+export default function Nav() {
+  return <div className="page">
+    <div className="nav">
+        <div className="nav-icons">
+            <img src="../src/assets/svg/paperclip.svg" alt="" />
+            <div className="vertical-separation"></div>
+            <div className="pages-nav">
+                <a className="footer-icon" href="">about me</a>
+         <a className="footer-icon" href="">projects</a>
+            </div>
+        </div>
+        <img className="menu" src="../src/assets/svg/menu.svg" alt="" />
+    </div>
+  </div>;
+}
