1c00b3ae114c7fd1e78ce564997ca000bc534801
eko0 <eko00.5177@gmail.com>
2025-11-19 23:01:57 +0100

    vbn gmhv

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,4 @@
-import { motion } from "framer-motion";
-import ThemeToggle from "./ui/items/ThemeToggle";
-import Footer from "./ui/component/Footer";
-import Nav from "./ui/component/Nav";
-import { body, path } from "framer-motion/client";
-import { useEffect } from "react";
-import React from "react";
+
 
 export default function Home() {
   return (
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,3 @@
-import { motion } from "framer-motion";
-import ThemeToggle from "./ui/items/ThemeToggle";
-import Footer from "./ui/component/Footer";
-import Nav from "./ui/component/Nav";
-import { body, path } from "framer-motion/client";
-import { useEffect } from "react";
-import React from "react";
-
 export default function Home() {
   return (
     <div className="page">
