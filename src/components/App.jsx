c659e4007f2c9eb701b5d2fb8334ff0c5a1f8318
Azzury <rodaucourt08@gmail.com>
2025-11-27 00:31:41 +0100

    Fixed pages access

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,4 @@
-import { motion } from "framer-motion";
-import { body, path } from "framer-motion/client";
-import { useEffect } from "react";
-import React from "react";
-
-export default function Home() {
+export default function App() {
   return (
     <div className="page">
       <div className="title">
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,7 @@
-import { motion } from "framer-motion";
-import { body, path } from "framer-motion/client";
-import { useEffect } from "react";
-import React from "react";
-
 export default function Project() {
   return (
     <div className="page">
-project
+      project
     </div>
   );
 }
